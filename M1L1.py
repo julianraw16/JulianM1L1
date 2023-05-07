940ff932f2ded51e44d7338cae767c5eeb732961
julianraw16 <132824567+julianraw16@users.noreply.github.com>
2023-05-07 19:44:47 +0700

    Update M1L1.py

--- M1L1.py
+++ M1L1.py
@@ -1,1 +1,11 @@
-
+meme_dict = {
+            "ROFL": "tanggapan terhadap lelucon",
+            "LITERALLY": "sinonim exactly yang memiliki arti secara tepat",
+            }
+for i in range(5):
+    word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+    if word in meme_dict.keys():
+        print(meme_dict[word])
+    else:
+        print("Kata kata tersebut tidak ada di kamus!")
+    
